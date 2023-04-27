65a8555493ba4d02892f7448be4e1cc5828a8702
Santiago Lara <you@example.com>
2023-04-27 17:02:11 -0500

    Maquetación página de estudiantes

--- src/ts/main.ts
+++ src/ts/main.ts
@@ -1,4 +1,12 @@
 import "../scss/main.scss";
+import { btnRemoveSession, modals } from "./nodes";
 import Token from "./token";
+import { Modal } from "bootstrap";
 
 !Token.validateSessionToken() ? window.location.replace("/login/") : null;
+
+btnRemoveSession.addEventListener("click", Token.removeSession);
+
+modals.forEach((modal) => {
+  new Modal(modal);
+});
---
+++ src/ts/nodes/index.ts
@@ -0,0 +1,17 @@
+export const btnCancel = document.getElementById("cancel-modal") as HTMLButtonElement;
+
+export const formStudent = document.querySelector("#form-student") as HTMLFormElement;
+
+export const formUpdateStudent = document.querySelector("#form-update-student") as HTMLFormElement;
+
+export const btnRemoveSession = document.getElementById("btn-cerrar-sesion") as HTMLButtonElement;
+
+export const tableBody = document.querySelector(".table-body") as HTMLTableElement;
+
+export const modals = document.querySelectorAll('[data-bs-toggle="modal"]');
+
+export const modalCreateStudent = document.getElementById("modal-create-student") as HTMLDivElement;
+
+export const modalUpdateStudent = document.getElementById("modal-update-student") as HTMLDivElement;
+
+export const notification = document.getElementById("notification") as HTMLDivElement;
